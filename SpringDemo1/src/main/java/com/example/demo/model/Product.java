f74ed9e1177e29ef2b0a040e7e132ee7664a71d1
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2025-04-26 19:11:25 +0530

    first commit

---
+++ SpringDemo1/src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+	
+	private int pid;
+	private String pname;
+	private int price;
+	private String description;
+	
+
+}
